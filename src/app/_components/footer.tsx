1402fefe57063c4c97f02ecd381363edfe4211fa
鸿鹄重工业株式会社 <133942878+honghuzhonggong@users.noreply.github.com>
2025-09-26 16:21:19 +0800

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,10 +4,10 @@ import { EXAMPLE_PATH } from "@/lib/constants";
 export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
-      {/* <Container>
+      <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Power by ABDEREMANE SOILIHI.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
@@ -16,15 +16,15 @@ export function Footer() {
             >
               Read Documentation
             </a>
-            <a
+            {/* <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
-            </a>
+            </a> */}
           </div>
         </div>
-      </Container> */}
+      </Container>
     </footer>
   );
 }
