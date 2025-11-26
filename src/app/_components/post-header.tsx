6ad8546ca7f59f6b8f5fbf01d09c54d552c8d4f5
鸿鹄重工业株式会社 <133942878+honghuzhonggong@users.noreply.github.com>
2025-11-26 20:58:51 +0800

    Update post-header.tsx

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -18,7 +18,8 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
       <div className="hidden md:block md:mb-12">
         <Avatar name={author.name} picture={author.picture} />
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
+      {/* 添加 max-w-3xl 限制图片宽度，mx-auto 实现居中 */}
+      <div className="mb-8 md:mb-16 sm:mx-0 max-w-3xl mx-auto">
         <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
