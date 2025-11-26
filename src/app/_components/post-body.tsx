a6ab0a5e13f6cd480f860f432a988501297b4900
鸿鹄重工业株式会社 <133942878+honghuzhonggong@users.noreply.github.com>
2025-11-26 21:36:59 +0800

    Update post-body.tsx

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -9,7 +9,7 @@ type Props = {
 export function PostBody({ content }: Props) {
   return (
     // 外层容器，控制整体宽度和居中，max-w-5xl 限制最大宽度，mx-auto 实现水平居中,更改此处可以修改正文的宽度，数字可以是234567，或者直接删除max-w-...这个类，或max-w-none
-    <div className="max-w-5xl mx-auto">
+    <div className="max-w-6xl mx-auto">
       <div
         className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
